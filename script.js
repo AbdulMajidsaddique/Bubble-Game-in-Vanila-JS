e78e5f4a2f91ab620b037ecc7db30a40ee94e77d
AbdulMajidsaddique <majidsaddique2222@gmail.com>
2024-10-24 23:09:08 +0500

    Complete

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var timer = 60;
-
+var timer = 30;
+var score = 0;
+var hitrn = 0;
+function increaseScore(){
+    score +=10;
+    document.querySelector('#scoreVal').textContent = score;
+}
 function getNewHit(){
-    var rn = Math.floor(Math.random()*10);
-    document.querySelector("#hitVal").textContent = rn;
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent = hitrn;
 }
 function makeBubble(){
     var clutter= "";
@@ -22,10 +27,21 @@ function runTimer(){
         }
         else{
             clearInterval(clrtimer);
+            document.querySelector('#panel-bottom').innerHTML = `<h1>Game Over</h1>`
         }
         
       },1000)
 }
+
+document.querySelector('#panel-bottom')
+.addEventListener('click',function(details){
+   var clickednumber = Number(details.target.textContent);
+   if(clickednumber === hitrn){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+   }
+})
 runTimer();
 makeBubble();
 getNewHit();
